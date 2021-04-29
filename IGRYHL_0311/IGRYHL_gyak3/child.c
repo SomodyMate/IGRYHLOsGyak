87bd678a37e85e5e951b5df547e0f1b179bc2e16
SomodyMate <79252004+SomodyMate@users.noreply.github.com>
2021-04-30 00:04:26 +0200

    Add files via upload

---
+++ IGRYHL_0311/IGRYHL_gyak3/child.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+    int i = 0;
+    for( i = 0; i < 5; i++ )
+    {
+        printf("Gazdicsko Marcell\n");
+        printf("WSMZR9\n");
+    }
+
+    return 0;
+}
---
+++ IGRYHL_0311/IGRYHL_gyak3/parent.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+    pid_t pidnum;
+    pidnum = fork();
+    if(pidnum == 0)
+        {
+            execl("./child.out", "child", NULL);
+        }
+    else
+        {
+            perror("Non-parent process.");
+        }
+
+    return 0;
+}
